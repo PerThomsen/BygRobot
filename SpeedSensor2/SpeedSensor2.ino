f41ec570b609f6be359c6e31452d298b378b4385
Per Thomsen <PerThomsen@users.noreply.github.com>
2018-01-23 21:01:28 +0100

    SpeedSensur2  rettet

--- SpeedSensor2/SpeedSensor2.ino
+++ SpeedSensor2/SpeedSensor2.ino
@@ -5,24 +5,65 @@
     -Tested at TechNode Protolabz
     -www.electroschematics.com/
     */
-    const int encoderIn = 8; // input pin for the interrupter 
+    const int encoderIn1 = 8; // input pin for the interrupter 
+    const int encoderIn2 = 9; // input pin for the interrupter 
     const int statusLED = 13; // Output pin for Status indicator
-    const int pulseOutput = 12; // Pulse output pin for external interfacing
-    int detectState=0; // Variable for reading the encoder status
-    void setup()
-    {
-       pinMode(encoderIn, INPUT); //Set pin 8 as input
+
+    int detectState1=0; // Variable for reading the encoder status
+    int detectState2=0; // Variable for reading the encoder status
+    int counter;
+    int lastState1;
+    int newState1;
+    int counter1;
+    int lastState2;
+    int newState2;
+    int counter2;
+
+    void setup() {
+       Serial.begin( 9600 );
+       pinMode(encoderIn1, INPUT); //Set pin 8 as input
+       pinMode(encoderIn2, INPUT); //Set pin 9 as input
        pinMode(statusLED, OUTPUT); //Set pin 13 as output
-       pinMode(pulseOutput, OUTPUT); // Set Pin 12 as output
     }
+    
     void loop() {
-       detectState=digitalRead(encoderIn);
-       if (detectState == HIGH) { //If encoder output is high
+       detectState1=digitalRead(encoderIn1);
+       detectState2=digitalRead(encoderIn2);
+       counter++;
+       
+       if (detectState1 == HIGH) { //If encoder output is high
           digitalWrite(statusLED, HIGH); //Turn on the status LED
-          digitalWrite(pulseOutput,HIGH); // Give a logic-High level output
-       }
-       else {
+          newState1 = 1;
+       } else {
           digitalWrite(statusLED, LOW); //Turn off the status LED
-          digitalWrite(pulseOutput,LOW); // Give a logic-Low level output
+          newState1 = 0;          
        }
+       if (lastState1 == newState1) {
+        if (lastState1 == 0) lastState1 = 1; else lastState1 = 0;
+        counter1++;
+        //Serial.println(counter);
+       }
+
+       if (detectState2 == HIGH) { //If encoder output is high
+          newState2 = 1;
+       } else {
+          newState2 = 0;          
+       }
+       if (lastState2 == newState2) {
+        if (lastState2 == 0) lastState2 = 1; else lastState2 = 0;
+        counter2++;
+        //Serial.println(counter);
+       }
+      if (counter1 != counter2) {
+        Serial.print("Cnt1: ");  
+        Serial.print(counter1);
+        Serial.print(" Cnt2: ");  
+        Serial.println(counter2);
+      }
+      
+      if (counter == 10) {
+        counter1 = 0;
+        counter2 = 0;
+        counter  = 0;
+      }
     }
