64aa39c4e3431938f6f235116639b1073a42f865
Per Thomsen <PerThomsen@users.noreply.github.com>
2018-01-29 20:16:59 +0100

    AOD omdøbt

--- interrupt_AOD/interrupt_AOD.ino
+++ interrupt_AOD/interrupt_AOD.ino
@@ -1,12 +1,15 @@
 #include <Scheduler.h>
 #include <Scheduler/Semaphore.h>
 //https://www.altomdata.dk/byg-en-robot-2/4
+
 Semaphore mutex;
 int value;
+int value2;
+
 /* Tasks */
 void first_task() {
   static int i = 1;
-  Serial.print("first_task");
+  Serial.print("first_task: ");
   mutex.wait();
   value = i;
   Serial.println(i);
@@ -16,18 +19,21 @@ void first_task() {
 }
 //Se også:  Sikker programudvikling med C++
 void second_task() {
-  Serial.print("second_task");
+  Serial.print("second_task: ");
   mutex.wait();
-  int i = value;
+  int j = value2;
   mutex.signal();
-  Serial.println(i);
+  Serial.println(j);
   delay(2000);
+  value2=j+10;
 }
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("setup");
   Scheduler.start(NULL, second_task);
 }
 void loop() {
   first_task();
 }
+
