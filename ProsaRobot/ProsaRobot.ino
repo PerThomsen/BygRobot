0cea0cc977207fe96cfe93598bfbbe5b554ff156
Per Thomsen <PerThomsen@users.noreply.github.com>
2018-02-07 07:33:18 +0100

    Nyt script til styring

--- ProsaRobot/ProsaRobot.ino
+++ ProsaRobot/ProsaRobot.ino
@@ -1,12 +1,29 @@
-#include <Scheduler.h>
-#include <Scheduler/Semaphore.h>
-#include <Ardumoto.h>
+/*
+ * Robotbyggeprojekt af 'Elektronik & Mekanik' februar 2018
+ * 
+ * Styring af trehjulet selvkørende robotbil. 
+ * Undgå forhindringer og undgå at køre ud over en kant
+ *  
+ * Sensorer:
+ * Ping-sensor (ultralyd) 1 placeret centralt fortil
+ * og 2 placeret i hvert af de forreste hjørner 
+ * 
+ * Program struktur:
+ *  GLOBAL SETTINGS
+ *  SETUP
+ *  LOOP (main)
+ *  functions:
+ *  DRIVE
+ *  (PRINT) debugging
+ *  PING STATE
+ *  IR STATE
+ *  MOTOR STATE
+ *  
+ */
 
-Ardumoto Moto;
+                                           //=============== GLOBAL SETTINGS
 
-Semaphore light_mutex;
-Semaphore motor_mutex;
-
+// Left motor
 #define MOTOR_L_PWM 11 // PIN D11 --> Motor B Input A --> MOTOR B+ / PWM Speed (IA2) GUL
 #define MOTOR_L_DIR 13 // PIN D13 --> Motor B Input B --> MOTOR B  / Direction (IB2) ORANGE
 // Right motor
@@ -22,37 +39,8 @@ Semaphore motor_mutex;
 #define M_FORWARD LOW
 #define M_REVERSE HIGH
 
-#define ANALOG_SENSOR_PIN A0
-#define DIGITAL_SENSOR_PIN 3
-#define LEDPIN 13
-
-const int dataIN = 2; //IR sensor INPUT
-
-unsigned long prevmillis; // To store time
-unsigned long duration; // To store time difference
-unsigned long refresh; // To store time for refresh of reading
-
-int rpm; // RPM value
-int light_value;
-
-boolean currentstate; // Current state of IR input scan
-boolean prevstate; // State of IR sensor in previous scan
-
-struct motor_command {
-  int left;
-  int right;
-};
-typedef struct motor_command MotorCommand;
-
-MotorCommand mc;
-
- 
 void setup() {
   Serial.begin( 9600 );
-  Serial.println("setup");
-  Moto.begin();
-  Scheduler.start(NULL, light_sensor);
-  Scheduler.start(NULL, control_motor);
 
   pinMode( MOTOR_L_DIR, OUTPUT );
   pinMode( MOTOR_L_PWM, OUTPUT );
@@ -63,10 +51,6 @@ void setup() {
   pinMode( MOTOR_R_PWM, OUTPUT );
   digitalWrite( MOTOR_R_DIR, LOW );
   digitalWrite( MOTOR_R_PWM, LOW );
-
-  pinMode(dataIN,INPUT);       
-  prevmillis = 0;
-  prevstate = LOW;  
 }
 
 void measureRMP() { 
@@ -83,12 +67,6 @@ void measureRMP() {
    }
   prevstate = currentstate; // store this scan (prev scan) data for next scan
   
-  // LCD Display
-  if( ( millis()-refresh ) >= 100 )  {
-       Serial.println(rpm);  
-    }
-
-}
 
 void stopMotor() {
       // always stop motors briefly before abrupt changes
@@ -132,36 +110,8 @@ int invertOurValue(int input) {
   return 255 - input;
 }
 
-void light_sensor() {
-  int switch_state = digitalRead(DIGITAL_SENSOR_PIN);  
-  if (switch_state == LOW) {
-    digitalWrite(LEDPIN, HIGH);
-  } else {
-    digitalWrite(LEDPIN, LOW);
-  }
-
-  light_mutex.wait();
-  light_value = analogRead(ANALOG_SENSOR_PIN);
-  light_mutex.signal();
-  delay(250);
-}
-
-void control_motor() {
-  Serial.print("control_motor ");
-  motor_mutex.wait();
-  // kopierer værdier - command_center overskriver dem
-  int left = mc.left;
-  int right = mc.right;
-  motor_mutex.signal();
-
-  Serial.println(left, DEC);
-  Serial.println(right, DEC);
-  Moto.setSpeed('A', left);
-  Moto.setSpeed('B', right);
-  delay(250);
-}
-
-void MOTORSTYR() {
+//Main
+void loop() {
   boolean isValidInput;
   int bias = 0; // Compensation to right motor  // draw a menu on the serial port
   Serial.println( "-----------------------------" );
@@ -214,45 +164,4 @@ void MOTORSTYR() {
   } while( isValidInput == true );
  
   // repeat the main loop and redraw the menu...
-}
-
-void command_center() {
-  const int max = 100;
-  static int last_proportional = 0;
-  static int integral = 0;
-  
-  unsigned int position;
-  light_mutex.wait();
-  position = light_value;
-  light_mutex.signal();
-  Serial.println(light_value, DEC);
-
-  int proportional = ((int)position) - 511; // lyssensor mellem 0 og 1023
-  int derivative = proportional - last_proportional;
-  integral += proportional;
-  last_proportional = proportional;
-
-  int power_difference = proportional/20 + integral/10000 + derivative*3/2;
-  Serial.print("power_difference = ");
-  Serial.println(power_difference, DEC);
-  if (power_difference > max)
-    power_difference = max;
-  if (power_difference < -max)
-    power_difference = -max;
-
-  motor_mutex.wait();
-  if (power_difference < 0) {
-    mc.left = max+power_difference;
-    mc.right = max;
-  } else {
-    mc.left = max;
-    mc.right = max-power_difference;
-  }
-  motor_mutex.signal();
-  
-  delay(500);
-}
-
-void loop() {
-  command_center();
 }
